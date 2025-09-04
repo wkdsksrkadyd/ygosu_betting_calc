9695baeca2c9b25815a03e03ddbb9d10e49f2e7a
wkdsksrkadyd <wkdsksrkadyd@gmail.com>
2025-09-04 16:37:19 +0900

    세트카 계산기 v1.07

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from app import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
-    app.run(debug=False)  # 배포할 땐 debug=False
+# if __name__ == "__main__":
+#     app.run(debug=False)  # 배포할 땐 debug=False
